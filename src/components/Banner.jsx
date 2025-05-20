68a7d2f6af5d1504cdc0cb3bb78265454b91fe12
vrodrigueskauan <kauanvenanciobarata@gmail.com>
2025-05-20 11:40:52 -0300

    aaa

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Particles from "./BackgroundParticles";
 import "./Banner.css";
+import BlurText from "./Blurtxt";
+import SplitText from "./SplitTxt";
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
+
+
 
 const Banner = () => {
   return (
@@ -12,9 +20,27 @@ const Banner = () => {
           alt="Imagem à esquerda"
           className="banner-image"
         />
-        <div className="banner-text">
-          <h1>Animosity</h1>
-          <p>Explorando emoções</p>
+         <div className="banner-text">
+    <SplitText
+      text="Animosity"
+      className="txt"
+      delay={150}
+      animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+      animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+      easing="easeOutCubic"
+      threshold={0.2}
+      rootMargin="-50px"
+      onLetterAnimationComplete={handleAnimationComplete}
+    />
+
+    <BlurText
+      text="Explorando emoções"
+      delay={150}
+      animateBy="words"
+      direction="top"
+      onAnimationComplete={handleAnimationComplete}
+      className="text-2xl mb-8"
+    />
           <button class="ui-btn">
           <span>
             Saiba Mais
