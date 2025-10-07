d3507361f09b5d63b3db64321c3ed71cdcd59fba
vrodrigueskauan <kauanvenanciobarata@gmail.com>
2025-10-07 18:57:49 -0300

    AAAAAAA

--- banco/banco_animosidade.sql
+++ banco/banco_animosidade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30/09/2025 às 00:48
+-- Tempo de geração: 07/10/2025 às 23:55
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `banco_animosidade`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `atualizacoes`
+--
+
+CREATE TABLE `atualizacoes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `versao` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `atualizacoes`
+--
+
+INSERT INTO `atualizacoes` (`id`, `titulo`, `descricao`, `versao`) VALUES
+(2, 'bolas', 'ahhahahaahahha', '1.12'),
+(3, 'Foda', 'Fodastico', '69');
 
 -- --------------------------------------------------------
 
@@ -77,8 +98,35 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `senha`, `adm`, `nome`, `foto`) VALUES
+(3, 'kauan@email.com', 'batmangg', 0, 'kauan', NULL),
+(4, 'kauan12@email.com', '$2b$10$scb1YoQik2rUwJcovTVyjO/A7ErckHjRLekjPEgMNTA9EhpJTa8Oq', 0, 'kauan12', NULL),
+(5, '', '$2b$10$/uh5uu0XSyoshr.tlLymVe8hhKEw0hgnYHTKjSAs95HZebUaKNe0i', 0, '', NULL),
+(12, 'adadad', '$2b$10$BXpqo7SJQpayPMrM5k/i8eA7oIuSsog2E3oeIHJbqncYtJu7iNddS', 0, 'qdadad', NULL),
+(17, 'kauan@ggmail.com', '$2b$10$1Pm0AL8w6Cy13vYFx8IdIex10e7V1.wfOso3/umMOt9JudgJMc/Ky', 0, 'kauan venancio', NULL),
+(19, 'kauan@gmail.com', '$2b$10$wQ884QjfYCAc8YO5Jxl1L.8FHKC7o/UFtZi3CPOa5yQLSzX2gS8Ou', 0, 'kauan731', NULL),
+(20, 'kauan@hotmail.com', '$2b$10$vvysy9CBDY09A7ftB7uZCuCz6WObYPXYe8P4qDMsu4uXwZMTO7GDu', 0, 'kauan', NULL),
+(21, 'bugabuga@gmailkkk', '$2b$10$l4Na8Ej6K7UsAH9oxXkJ6.JNQywikPxcO8xW5tCz0wOdxzK22Cb.2', 0, 'rere', NULL),
+(23, 'kauan1@hotmail.com', '$2b$10$RtGFHmazopzW..KmsHMuvesz5cmkhn34d9B2ExIfaVpbPwVYhwvDe', 0, 'kauan', NULL),
+(24, 'kauan12@hotmail.com', '$2b$10$hL4e34SQNXeDjbMYx8dIOuh1LxGD0U3Kz1Z5.4/By/hR7UdST7u1q', 0, 'kauan', NULL),
+(25, 'joao@email.com', '$2b$10$fEPQpZ4LTBLv8O8HduIhJ..AnDhEvK9JBD8OKnqEqhBlDGNyQv7ze', 0, 'joao', NULL),
+(26, 'KAUAHDD@gmail.com', '$2b$10$09qBfaS0z9vNaDGHUT9gzOFp9wKJojuSFBDh6GY699DxAxsPCvMDu', 0, '1234', NULL),
+(27, 'kauanzin@gmail.com', '$2b$10$/YpAEAmpBddlS0aEin90lO0foPoMJiOEaS4fotavXuuyzVNGPe3qG', 0, 'kauan1234', NULL),
+(28, 'joaogay@gmail.com', '$2b$10$HiwJSfoTA5IOkyj25Rn7pewjLEOJVpDaA5r808xkKQArbAngNaYqy', 0, 'mini megacavaleiro', 0x2f75706c6f6164732f313735393736373235363036332d32313933363730352e706e67),
+(29, 'seila@gmail.com', '$2b$10$ie3V16PB5sPuj9ar.zxfa.Ghhv3S1pIFa9n9sm2H.4Y0BQpOz9.1m', 0, 'baitolas', 0x2f75706c6f6164732f313735393738343331363436352d33303031333630322e706e67);
+
+--
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `atualizacoes`
+--
+ALTER TABLE `atualizacoes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `feedback`
@@ -98,10 +146,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `atualizacoes`
+--
+ALTER TABLE `atualizacoes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Restrições para tabelas despejadas
