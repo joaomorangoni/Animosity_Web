131baf7043c74ccd4e94e48f593436598afa6977
vrodrigueskauan <kauanvenanciobarata@gmail.com>
2025-10-08 08:33:21 -0300

    aaaa

--- banco/banco_animosidade.sql
+++ banco/banco_animosidade.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/10/2025 às 23:55
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 08/10/2025 às 13:32
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `banco_animosidade`
 --
+CREATE DATABASE IF NOT EXISTS `banco_animosidade` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `banco_animosidade`;
 
 -- --------------------------------------------------------
 
